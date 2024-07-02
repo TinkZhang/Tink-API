2a728445984cd290576202a3eda0f5bff0132b41
TinkZhang <ZhangYunfengZJU@gmail.com>
2024-07-03 00:27:29 +0800

    test

--- myapp.py
+++ myapp.py
@@ -2,5 +2,11 @@ from flask import Flask
 from apis import api
 
 app = Flask(__name__)
-api.init_app(app)
+
+
+# api.init_app(app)
 # app.run()
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
