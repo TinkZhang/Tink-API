9df6e7f2840aea82d220ec9f3672c1b4004d9abc
TinkZhang <ZhangYunfengZJU@gmail.com>
2024-07-03 01:41:07 +0800

    test

--- myapp.py
+++ myapp.py
@@ -1,28 +1,6 @@
 from flask import Flask
-from flask_restx import Api, Resource
-
-import bus_data
+from apis import api
 
 app = Flask(__name__)
-api = Api(app)
-
-
-@api.route("/")
-class HelloWorld(Resource):
-    def get(self):
-        return "<p>Hello, World!</p>"
-
-
-@app.route("/version")
-def version():
-    return {"version": f"{bus_data.latestVersion}"}
-
-
-@app.route("/lines")
-def lines():
-    return bus_data.lines
-
-
-@app.route("/line/<line_id>")
-def line(line_id):
-    return [line for line in bus_data.lines if line["lineId"] == line_id]
+api.init_app(app)
+# app.run()
