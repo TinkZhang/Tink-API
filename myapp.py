73a0e23c44606fbec9e406029546bbb1e4c2de93
TinkZhang <ZhangYunfengZJU@gmail.com>
2024-07-03 09:18:21 +0800

    add api index

--- myapp.py
+++ myapp.py
@@ -3,4 +3,12 @@ from apis import api
 
 app = Flask(__name__)
 api.init_app(app)
-# app.run()
+
+
+@app.route('/')
+def index():
+    return {
+        "bus_version_url": "https://api.tinks.app/bus/version",
+        "bus_all_lines_url": "https://api.tinks.app/bus/lines",
+        "bus_line_url": "https://api.tinks.app/bus/{line_id}",
+    }
